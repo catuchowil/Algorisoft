1d702be9de10746b9939a7c7a7037cfb496c0f0c
catuchowil <gapuentes@hotmail.com.ar>
2022-03-19 20:10:27 -0300

    Modelos y listado de categorias

--- core/erp/urls.py
+++ core/erp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from .views import category_list
+
 urlpatterns = [
-    
+    path('category/list/',category_list, name='category_list')
 ]
--- core/erp/views.py
+++ core/erp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from.models import Category
+
 # Create your views here.
+def category_list(request):
+    data = {
+        'title':'Listado de Categorías',
+        'categories':Category.objects.all()
+    }
+    return render(request, 'erp/list.html',data)
