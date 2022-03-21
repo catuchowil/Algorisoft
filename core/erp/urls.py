70ffb7061fd9f884d9fa16c107b049e94e102da9
catuchowil <gapuentes@hotmail.com.ar>
2022-03-20 21:07:28 -0300

    Hatas el video 25 de Ajax

--- core/erp/urls.py
+++ core/erp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     #path('category/list/',category_list, name='category_list'),
 
     path('category/list/',CategoryListView.as_view(), name='category_list'),
+    path('category/list2/',category_list, name='category_list2'),
 ]
--- core/erp/views.py
+++ core/erp/views.py
@@ -1,8 +1,15 @@
 from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from django.views.decorators.csrf import csrf_protect, csrf_exempt
+
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+
 from django.views import generic
 
 from.models import Category
+from django.http import JsonResponse
 
 # Create your views here.
 def category_list(request):
@@ -18,6 +25,21 @@ class CategoryListView(generic.ListView):
     model = Category
     template_name = 'erp/list.html'
     context_object_name = 'obj'
+
+    #--Sobreescribo el método dispatch
+    @method_decorator(csrf_exempt)
+    def dispatch(self,request,*args, **kwargs):
+        return super().dispatch(request,*args, **kwargs)
+    
+
+    def post(self,request,*args, **kwargs):
+        data = {}
+        try:
+            data = Category.objects.get(pk=request.POST['id']).toJSON()
+        except Exception as e:
+            data['error'] = str(e)
+        return JsonResponse(data)
+
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
